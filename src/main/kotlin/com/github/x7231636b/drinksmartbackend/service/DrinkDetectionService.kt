a9f063496dc1c78dfb41336a79e215a186fc3eae
Markus Hallmann <markus@hallmann.pw>
2024-04-07 13:10:37 +0200

    More restructuring and implementing a put endpoint to modify entries

--- src/main/kotlin/com/github/x7231636b/drinksmartbackend/service/DrinkDetectionService.kt
+++ src/main/kotlin/com/github/x7231636b/drinksmartbackend/service/DrinkDetectionService.kt
@@ -1,24 +1,50 @@
 package com.github.x7231636b.drinksmartbackend.service
 
+import DrinkData
+import GetVolumeForDayResponse
 import com.github.x7231636b.drinksmartbackend.entity.DrinkDataEntity
 import com.github.x7231636b.drinksmartbackend.repository.DrinkDetectionRepository
 import com.github.x7231636b.drinksmartbackend.util.isSameDay
+import java.util.UUID
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class DrinkDetectionService(private val drinkDetectionRepository: DrinkDetectionRepository) {
 
-  fun saveDrinkDataEntity(DrinkDataEntity: DrinkDataEntity) {
-    drinkDetectionRepository.save(DrinkDataEntity)
+  private val log: Logger = LoggerFactory.getLogger(DrinkDetectionService::class.java)
+
+  fun saveDrinkData(drinkData: DrinkData) {
+    drinkDetectionRepository.save(DrinkDataEntity.fromDto(drinkData))
   }
 
-  fun getDrinkDataEntity(userName: String): List<DrinkDataEntity> {
+  fun updateDrinkData(drinkData: DrinkData) {
+    drinkDetectionRepository.findById(UUID.fromString(drinkData.id)).ifPresent { existingEntity ->
+      val updatedEntity =
+          existingEntity.copy(
+              id = existingEntity.id,
+              userName = drinkData.userName,
+              timeStamp = drinkData.timeStamp,
+              volume = drinkData.volume
+          )
+      drinkDetectionRepository.save(updatedEntity)
+    }
+  }
+
+  fun getDrinkData(userName: String): List<DrinkDataEntity> {
     return drinkDetectionRepository.findAllByUserName(userName)
   }
 
-  fun getDrinkDataEntity(userName: String, date: Long): List<DrinkDataEntity> {
-    return drinkDetectionRepository.findAllByUserName(userName).filter {
-      isSameDay(it.timeStamp, date)
-    }
+  fun getVolumeForDay(userName: String, date: Long?): GetVolumeForDayResponse {
+    val dateToUse = date ?: System.currentTimeMillis()
+    var list =
+        drinkDetectionRepository.findAllByUserName(userName).filter {
+          isSameDay(it.timeStamp, dateToUse)
+        }
+    var sips = list.size
+    var volume = list.map { it.volume }.sum()
+    var response = GetVolumeForDayResponse(userName, dateToUse, volume, sips)
+    return response
   }
 }
